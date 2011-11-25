8fb820cd0b59b5187c394c0cb618325159afa319
Stephen Bryant <sbryant31@gmail.com>
2011-11-25 16:33:25 -0500

    Switched from outputting to CSV to outputting to pyplots

--- README.txt
+++ README.txt
@@ -4,3 +4,4 @@ Stephen Bryant. 11/23/2011
 
 Uses a genetic algorithm to tune gain values for a PID controller for abstract
 1-dimensional line following.
+
--- genetic_pid.py
+++ genetic_pid.py
@@ -129,22 +129,32 @@ Run simulation for a specific chromosome c.
 
 Returns the fitness function value of the simulation
 """
-def run_simulation_for_best(map, population, chromosome, runNumber, fitness_factor):
+def run_simulation_for_champion(map, population, chromosome, runNumber, fitness_factor):
 	
 	distance_list = []
 	current_position = 0
 	last_distance = 0
 	current_summation = 0
 	current_distance = 0
-					
-	csvWriter = csv.writer(open('best_run' + str(runNumber) + '.csv', 'wb'), delimiter=',',
-							quotechar='|', quoting=csv.QUOTE_MINIMAL)
+	
+	import numpy as np
+	import matplotlib.pyplot as plt
+	
+	plt.figure()
+	plt.plot()
+	plt.title("Best run in run number " + str(runNumber))
+	
+	#csvWriter = csv.writer(open('best_run' + str(runNumber) + '.csv', 'wb'), delimiter=',',
+	#						quotechar='|', quoting=csv.QUOTE_MINIMAL)
 						
-	csvWriter.writerow(['fitness ', fitness_factor])
-	csvWriter.writerow(['kp', population[chromosome].kp])
-	csvWriter.writerow(['kd', population[chromosome].kd])
-	csvWriter.writerow(['ki', population[chromosome].ki])
-	csvWriter.writerow(['position: ', 'line position'])					
+	#csvWriter.writerow(['fitness ', fitness_factor])
+	#csvWriter.writerow(['kp', population[chromosome].kp])
+	#csvWriter.writerow(['kd', population[chromosome].kd])
+	#csvWriter.writerow(['ki', population[chromosome].ki])
+	#csvWriter.writerow(['position: ', 'line position'])		
+
+	
+	positions = []
 					
 	for time in range(MAX_TIMESTEPS):
 		distance_list.append(time)
@@ -159,15 +169,23 @@ def run_simulation_for_best(map, population, chromosome, runNumber, fitness_fact
 		#new_velocity = new_velocity * (1 + random.random()*DYNAMIC_FACTOR)
 	
 		current_position = current_position + new_velocity
-			
-		csvWriter.writerow([current_position, map[time]])
+		
+		#csvWriter.writerow([current_position, map[time]])
+		positions.append(time)
+		positions[time] = current_position
 		
 		distance_list[time] = current_distance
 			
 		# for the derivative
 		last_distance = current_distance
 		
-
+	# plot positions of champion versus time.
+	plt.plot(range(MAX_TIMESTEPS), positions, label = r"Robot positions")
+	plt.plot(range(MAX_TIMESTEPS), map, label = r"Line positions")
+	plt.legend(loc='upper right')
+	plt.xlabel("Time")
+	plt.ylabel("Position")
+	plt.show()
 	
 	return fitness(distance_list)
 	
@@ -305,34 +323,74 @@ def generate_new_population(fitness_values, previous_population):
 #   we will pick the wrong index when generating a new population. Which would be bad.
 #
 #################################################################################################################################################
+
 # create map or load map based on debug mode
 #map = create_map()
 map = load_map()
 population = generate_initial_population()
 fitness_values = run_simulation(map, population)
 
+max_values = []
+avg_values = []
+kp_values = []
+kd_values = []
+ki_values = []
 
-# write out our results to a csv for graphing
-csvWriter = csv.writer(open('geneticalgo.csv', 'wb'), delimiter=',',
-							quotechar='|', quoting=csv.QUOTE_MINIMAL)
+# perform simulation
+
 for i in range(MAX_RUNS):
+
+	# generate population and run the simulation
 	population = generate_new_population(fitness_values, population)
 	fitness_values = run_simulation(map, population)
 	
+	# add the champion chromosome to a list of champions for plotting
+	index_of_champion = listtools.max_index_in_list(fitness_values)
+	kp_values.append(i)
+	kp_values[i] = population[index_of_champion].kp
+	kd_values.append(i)
+	kd_values[i] = population[index_of_champion].kd
+	ki_values.append(i)
+	ki_values[i] = population[index_of_champion].ki
+	
+
+	# add the max/average values to lists for plotting
+	max_values.append(i)
+	avg_values.append(i)
+	max_values[i] = listtools.max_value_in_list(fitness_values)
+	avg_values[i] = listtools.avgList(fitness_values)
+	
+	
+	# every 10 runs, do a plot of the champion chromosome
 	if i % 10 == 0:
 		# run the simulation for the first selected parent
-		run_simulation_for_best(map, population, listtools.max_index_in_list(fitness_values), i, listtools.max_value_in_list(fitness_values))
-	
-	max_value = listtools.max_value_in_list(fitness_values)
-	avg_value = listtools.avgList(fitness_values)
-	
-	# if the population sucks, DESTROY THE EARTH
-	if max_value < FITNESS_THRESHOLD:
-		population = generate_initial_population()
-		continue
+		run_simulation_for_champion(map, population, index_of_champion, i, listtools.max_value_in_list(fitness_values))
 
-	csvWriter.writerow([avg_value, max_value])
-	print "Run " + str(i) + ": max value " + str(max_value) + ", avg value " + str(avg_value)
+	print "Run " + str(i) + ": max value " + str(max_values[i]) + ", avg value " + str(avg_values[i])
 
-		
+import matplotlib.pyplot as plt
 	
+# plot fitness results of each run	
+plt.figure()
+plt.plot()
+plt.title("Fitness Values Over Time")
+
+plt.plot(range(MAX_RUNS), max_values, label = r"Max Value")
+plt.plot(range(MAX_RUNS), avg_values, label = r"Average Value")
+plt.legend(loc='bottom right')
+plt.xlabel("Run")
+plt.ylabel("Value")
+plt.show()		
+
+# plot values of parameters for each run
+plt.figure()
+plt.plot()
+plt.title("Champion Gain Values Per Run")
+
+plt.plot(range(MAX_RUNS), kp_values, label = r"Kp")
+plt.plot(range(MAX_RUNS), kd_values, label = r"Kd")
+plt.plot(range(MAX_RUNS), ki_values, label = r"Ki")
+plt.legend(loc='lower center')
+plt.xlabel("Run")
+plt.ylabel("Value")
+plt.show()		
