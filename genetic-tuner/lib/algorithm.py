b1cc4c22acc2cc14dc80efd76312633c893b0b97
Billy <34207387+PouryaParsa@users.noreply.github.com>
2023-12-27 14:16:24 +0330

    Update algorithm.py

--- genetic-tuner/lib/algorithm.py
+++ genetic-tuner/lib/algorithm.py
@@ -1,7 +1,7 @@
 import random
 import math
-import listtools
 from chromosome import Chromosome
+from listtools import normListSumTo
 
 class GeneticAlgorithm:
     def __init__(self, config):
@@ -9,89 +9,52 @@ class GeneticAlgorithm:
 
     def mutation(self, chromosome):
         """
-        With a mutation probability mutate new offspring at each locus (position in chromosome).
+        Mutate the chromosome with a mutation probability at each locus.
         """
-        random.seed()
-        #very small real valued mutation
         max_gain_value = self.config['max_gain_value']
+        mutation_probability = self.config['mutation_probability']
 
-        if random.random() < self.config['mutation_probability'] / 3:
-            chromosome.kp = chromosome.kp + random.random()/max_gain_value
-        if chromosome.kp < 0:
-            chromosome.kp = random.random() * max_gain_value
+        def apply_mutation(gene, mutation_prob):
+            if random.random() < mutation_prob:
+                gene += random.random() / max_gain_value
+            if gene < 0:
+                gene = random.random() * max_gain_value
+            return gene
 
-        elif random.random() < self.config['mutation_probability'] * 2/3:
-            chromosome.ki = chromosome.ki + random.random()/max_gain_value
-        if chromosome.ki < 0:
-            chromosome.ki = random.random() * max_gain_value
-
-        elif random.random() < self.config['mutation_probability']:
-            chromosome.kd = chromosome.kd + random.random()/max_gain_value
-        if chromosome.kd < 0:
-            chromosome.kd = random.random() * max_gain_value
+        chromosome.kp = apply_mutation(chromosome.kp, mutation_probability / 3)
+        chromosome.ki = apply_mutation(chromosome.ki, mutation_probability * 2 / 3)
+        chromosome.kd = apply_mutation(chromosome.kd, mutation_probability)
 
         return chromosome
 
     def crossover(self, parent1, parent2):
         """
-        3b[Crossover] With a crossover probability cross over the parents to form a new offspring (children).
-        If no crossover was performed, offspring is an exact copy of parents.
+        Perform crossover to form a new offspring (child).
         """
-        random.seed()
-
-        # if we dont crossover, offspring is a copy of parents
         if random.random() > self.config['crossover_rate']:
             return parent1
         else:
-            # random combination crossover
-            number = random.random()
-            if number < 1.0/6:
-                return Chromosome(parent1.kp,parent1.kd, parent1.ki)
-            elif number < 2.0/6:
-                return Chromosome(parent2.kp, parent1.kd, parent1.ki)
-            elif number < 3.0/6:
-                return Chromosome(parent1.kp, parent2.kd, parent1.ki)
-            elif number < 4.0/6:
-                return Chromosome(parent1.kp, parent1.kd, parent2.ki)
-            elif number < 5.0/6:
-                return Chromosome(parent2.kp,parent1.kd, parent2.ki)
-            else:
-                return Chromosome(parent2.kp,parent2.kd, parent2.ki)
+            crossover_types = [
+                (parent1.kp, parent1.kd, parent1.ki),
+                (parent2.kp, parent1.kd, parent1.ki),
+                (parent1.kp, parent2.kd, parent1.ki),
+                (parent1.kp, parent1.kd, parent2.ki),
+                (parent2.kp, parent1.kd, parent2.ki),
+                (parent2.kp, parent2.kd, parent2.ki)
+            ]
+            return Chromosome(*random.choice(crossover_types))
 
     def selection(self, fitness_values):
         """
-        Pick two parents according to probability represented by normalized fitness values
-        3a[Selection] Select two parent chromosomes from a population according to their fitness (the better fitness, the bigger chance to be selected)
+        Select two parent indices based on normalized fitness values.
         """
-        # normalize the list so we have probabilities to pick parents
-        fitness_values = listtools.normListSumTo(fitness_values, 1)
-        parentIndices = []
-        random.seed()
-        parent1_probability = random.random()
-        parent2_probability = random.random()
-
-        sum = 0
-        for i in range(self.config['population_size']):
-            if len(parentIndices) == 2:
-                break
-            next_sum = sum + fitness_values[i]
-            if parent1_probability <= next_sum and parent1_probability >= sum:
-                parentIndices.append(i)
-            if parent2_probability <= next_sum and parent2_probability >= sum:
-                parentIndices.append(i)
-            sum = next_sum
-        return parentIndices
+        fitness_values = normListSumTo(fitness_values, 1)
+        parent_indices = random.choices(range(len(fitness_values)), weights=fitness_values, k=2)
+        return parent_indices
 
     def fitness(self, distance_list):
         """
-        Evaluate the fitness f(x) of each chromosome x in the population
-        returns the fitness value according to the fitness function
-
-        f(x): takes in a list of distances, finds the sum of their squares and returns the inverse of it.
+        Evaluate the fitness of each chromosome in the population.
         """
-        sum = 0
-        for i in range(len(distance_list)):
-            sum = sum + distance_list[i]**2
-        return 1 /  math.sqrt(sum)
-
-
+        sum_squares = sum(distance ** 2 for distance in distance_list)
+        return 1 / math.sqrt(sum_squares)
