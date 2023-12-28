10867530aa2a3e218aba154fab5c057108478f5b
Billy <34207387+PouryaParsa@users.noreply.github.com>
2023-12-28 14:04:04 +0330

    Update config.py

--- genetic-tuner/config.py
+++ genetic-tuner/config.py
@@ -1,18 +1,16 @@
-config = {
-    'population_size' : 100,
-    'mutation_probability' : .1,
-    'crossover_rate' : .9,
-    # maximum simulation runs before finishing
-    'max_runs' : 100,
-    # maximum timesteps per simulation
-    'max_timesteps' : 150,
-    # smoothness value of the line in [0, 1]
-    'line_smoothness' : .4,
-    # Bound for our gain parameters (p, i, d)
-    'max_gain_value' : 3,
-    # when set to 1, we create a new map this run. When set to 0, loads a new map
-    'new_map' : True,
-    'runs_per_screenshot' : 10,
-    'data_directory' : '/home/monk/genetic_pid_data',
-    'map_filename' : 'map.csv'
-}
+class Config:
+    def __init__(self):
+        self.population_size = 100
+        self.mutation_probability = 0.1
+        self.crossover_rate = 0.9
+        self.max_runs = 100
+        self.max_timesteps = 150
+        self.line_smoothness = 0.4
+        self.max_gain_value = 3
+        self.new_map = True
+        self.runs_per_screenshot = 10
+        self.data_directory = '/home/monk/genetic_pid_data'
+        self.map_filename = 'map.csv'
+
+# Create an instance of the Config class
+config = Config()
